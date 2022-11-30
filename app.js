a7f4ced1432599dc49eea6e851d1837314615a0a
Hafiz Sheraz Hanif Khan <[200~hafizsherazhanif@gmail.com~>
2022-11-30 20:18:09 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,89 @@
+//Async Function
+//Async function always return promise
+//Await function jo hy wo await le ata hy Yani ke gap
+
+//Map Method
+
+// Map, Reduce , Filter
+// Map method array me ek new array deta hy
+// Filter method array me ek condition ki surat me yani less then or greater me ek new array deta hai
+// Reduce method array me jo value hoti hy isme value add krta hai ek ke bd ek yani function argument
+// Call method
+// call method me ek object apne dosre object ko use kr sakta hai
+// call methos argument ko seperately leta hai.Jbke apply method argument ko array form me leta hai
+
+//Closures
+
+// Closures
+// Jb hm ek function ke andar dosra function banaty hy to isme jo phla function hota hy issko outer function blty hai or iska jo dosra function hota hai isko inner function blty hai
+// Closures ka mtlb hy ke function apne out side scope ko access kr sakta hai
+// Closures is a combination of function
+// function scope
+
+
+//Javascript concept
+
+// li.appendchild(liText)
+// iska matlab hy ke li ke andar li ke text ki value add krdo
+// document.createelement
+// ke zariye hm html ka tag create kr sakty hy javascript se
+// document.createtextnode se li ya kisi bhi tag ke andar ka data bhi console me randar krwa sakty hy hm
+// html ke element me attribute set krta hy
+// Var ke variable me hm value ko declare bhi kr sakty hy or reassign bhi kr sakty hai
+// let ke variable me hm value ko declare nhi kr sakty hy bilkul value isme redeclear nhi hoti pr reassign kr sakty hai
+// jabki cons ke variable me declear or reasign dono hi nhi kr sakty
+// or jb hm var valaible me if ki condition lagay gy
+// if(condition){
+// var x="hello"}
+// document.write(x) var ka scope global scope hy
+// or jb ye condition hm let me lagaye gy to hamare pass ek error ajygi or let ka scope block scope hai
+// var ki condition curly bracket me bhi chlygi jbke let ki condition curly bracket me nhi chly gi
+// let or cons dono block scope hy
+// Arrow Function topics but this is simple function syntax
+// function hello (){
+// console.log("hello")
+// } hello()
+// let hello=function(){
+// console.log("Hello")
+// } hello()
+// THis is arrow funcion syntax let hello=()=>console.log("hello");
+// hello()
+// or ye bhi syntax hy arrow function ka
+// let welcome=(name,age)=>'Welcome ${name} -${age};
+// console.log(welcome("Yahoo Baba",25));
+// agar arrow function me parameter one ho two na ho to apko round bracket bhi lagane ki zarorat nhi hy
+
+//Set in javacript
+// Set in javascript
+// Set stores unique values
+// set jo hy wo value ko repeat nhi hone deta hy
+
+
+//Lexical scope
+
+//Lexical scope ka mtlb hy ke jitne bhi inner function hy wo apne parent ki value ko access kr sakta hy
+
+
+//Event Bubbling
+
+
+// Event bubbling me by defalut value false hoti hy
+// The default value is false,which will use the bubbling propagation when the value is set to true,the event uses the capturing propagation
+// Capturing me parent phly dekhyga or parent se child pr hoga bubbling
+// Jabke bubbling me child se parent pr hoga Bubbling
+// Agar hm event bubbling me chahy ke sirf button pr click kary to button ay div pr click kary to div ay to isme hamare pass ek special function ata hy iska naam hy
+// Event.stopPropagation
+// Block Scope
+// Ek block level ke andar kafi function scope ho sakty hai
+// Block scope let or cons ki waja se introduce krway gaye thy jb Ecmascript 2015 me aya
+// let or const variable block scope hy
+
+//"This is seriously very hard"
+
+// This changes is very best
+
+
+
+
+
+
